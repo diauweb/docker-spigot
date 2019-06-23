f53eff9dcd39f9f12b3e2c427749348b53a6ef86
Xiaodai Dai <diauweb@live.com>
2019-06-23 17:34:39 +0800

    Support for /restart command

--- server_init.sh
+++ server_init.sh
@@ -7,11 +7,27 @@ echo "## https://account.mojang.com/documents/minecraft_eula  ##"
 echo "eula=TRUE" > $SPIGOT_HOME/eula.txt
 
 # don't directly copy file into volumes during build
-if [ ! -f $SPIGOT_HOME/spigot.jar ]; then
-    echo ">> Spigot jar does not exist, copy from image build"
-    cp /tmp/spigot.jar $SPIGOT_HOME/spigot.jar
+if [ -f $SPIGOT_HOME/spigot.jar ]; then
+    echo ">> Spigot jar exists, cleaning..."
+    rm -f $SPIGOT_HOME/spigot.jar
+fi
+cp /tmp/spigot.jar $SPIGOT_HOME/spigot.jar
+
+if [ ! -f $SPIGOT_HOME/restart.sh ]; then
+    echo "#!/bin/bash" > $SPIGOT_HOME/restart.sh
+    echo "echo 1 > /tmp/spigot-restart.wanted" >> $SPIGOT_HOME/restart.sh
+    chmod +x /tmp/spigot-restart.wanted
 fi
 
 cd $SPIGOT_HOME
 
+# Deal with restarts
+echo 1 > /tmp/spigot-restart.wanted
+
+while [ -f /tmp/spigot-restart.wanted ]
+do
+rm -f /tmp/spigot-restart.wanted
 /opt/mcdaemon.d/server_run.sh java $JVM_OPTS -jar spigot.jar
+done
+
+
